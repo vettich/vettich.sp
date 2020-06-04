2c2e1fcb984a474e58aae72ffd0f4edcc26224b8
Vetti <vetti.ch@ya.ru>
2020-06-05 00:00:26 +0500

    moved devform from install/bitrix/module to install/module

--- install/index.php
+++ install/index.php
@@ -1,37 +1,37 @@
-<?
+<?php
 IncludeModuleLangFile(__FILE__);
 class vettich_sp extends CModule
 {
 	const MODULE_ID = 'vettich.sp';
-	var $MODULE_ID = 'vettich.sp';
-	var $MODULE_VERSION;
-	var $MODULE_VERSION_DATE;
-	var $MODULE_NAME;
-	var $MODULE_DESCRIPTION;
-	var $MODULE_GROUP_RIGHTS = 'Y';
-	var $MODULE_ROOT_DIR = '';
+	public $MODULE_ID = 'vettich.sp';
+	public $MODULE_VERSION;
+	public $MODULE_VERSION_DATE;
+	public $MODULE_NAME;
+	public $MODULE_DESCRIPTION;
+	public $MODULE_GROUP_RIGHTS = 'Y';
+	public $MODULE_ROOT_DIR = '';
 
-	function vettich_sp()
+	public function vettich_sp()
 	{
-		$arModuleVersion = array();
+		$arModuleVersion = [];
 		include(__DIR__.'/version.php');
-		if(is_array($arModuleVersion) && array_key_exists('VERSION', $arModuleVersion)){
+		if (is_array($arModuleVersion) && array_key_exists('VERSION', $arModuleVersion)) {
 			$this->MODULE_VERSION = $arModuleVersion['VERSION'];
 			$this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
 		}
 		$this->MODULE_ROOT_DIR = dirname(__DIR__);
 		$this->MODULE_NAME = GetMessage('VETTICH_SP_MODULE_NAME');
 		$this->MODULE_DESCRIPTION = GetMessage('VETTICH_SP_MODULE_DESCRIPTION');
-		$this->PARTNER_NAME = GetMessage('VETTICH_SP_PARTNER_NAME'); 
+		$this->PARTNER_NAME = GetMessage('VETTICH_SP_PARTNER_NAME');
 		$this->PARTNER_URI = GetMessage('VETTICH_SP_PARTNER_URI');
 	}
 
-	function DoInstall()
+	public function DoInstall()
 	{
 		global $DOCUMENT_ROOT, $APPLICATION, $errors, $ver, $GLOBALS;
 		$GLOBALS['CACHE_MANAGER']->CleanAll();
 		$this->InstallDevform();
-		if($this->InstallDB()
+		if ($this->InstallDB()
 			&& $this->InstallFiles()
 			&& $this->InstallEvents()) {
 			RegisterModule(self::MODULE_ID);
@@ -41,32 +41,33 @@ class vettich_sp extends CModule
 		return false;
 	}
 
-	function InstallDevform()
+	public function InstallDevform()
 	{
-		if(CModule::IncludeModule('vettich.devform')) {
+		if (CModule::IncludeModule('vettich.devform')) {
 			return;
 		}
-		if(!file_exists($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/vettich.devform/install/index.php')) {
-			CopyDirFiles($this->MODULE_ROOT_DIR.'/install/bitrix/modules',$_SERVER['DOCUMENT_ROOT'].'/bitrix/modules', true, true);
+		if (!file_exists($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/vettich.devform/install/index.php')) {
+			CopyDirFiles($this->MODULE_ROOT_DIR.'/install/modules', $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules', true, true);
 		}
 		include $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/vettich.devform/install/index.php';
-		if(class_exists('vettich_devform')) {
+		if (class_exists('vettich_devform')) {
 			$cl = new vettich_devform();
-			if(!$cl->IsInstalled()) {
+			if (!$cl->IsInstalled()) {
 				$cl->DoInstall();
 			}
 		}
 	}
 
-	function DoUninstall(){
+	public function DoUninstall()
+	{
 		global $DOCUMENT_ROOT, $APPLICATION, $step;
 		$step = IntVal($step);
-		if($step<2) {
+		if ($step<2) {
 			$APPLICATION->IncludeAdminFile(GetMessage('VETTICH_SP_UNINSTALL_TITLE'), $this->MODULE_ROOT_DIR.'/install/unstep1.php');
-		} elseif($step==2) {
-			if($this->UnInstallDB(array(
+		} elseif ($step==2) {
+			if ($this->UnInstallDB([
 					'savedata' => $_REQUEST['savedata'],
-				))
+				])
 				&& $this->UnInstallFiles()
 				&& $this->UnInstallEvents()) {
 				UnRegisterModule(self::MODULE_ID);
@@ -76,25 +77,25 @@ class vettich_sp extends CModule
 		}
 	}
 
-	function InstallDB($arModuleParams = array())
+	public function InstallDB($arModuleParams = [])
 	{
 		$lib = $this->MODULE_ROOT_DIR.'/lib';
 		include $lib.'/db/dbase.php';
 		include $lib.'/db/post.php';
-		if(!Vettich\SP\db\postTable::createTable()) {
+		if (!Vettich\SP\db\postTable::createTable()) {
 			return false;
 		}
 
 		include $lib.'/db/postaccount.php';
-		if(!Vettich\SP\db\postAccountTable::createTable()) {
+		if (!Vettich\SP\db\postAccountTable::createTable()) {
 			return false;
 		}
 		include $lib.'/db/pending.php';
-		if(!Vettich\SP\db\pendingTable::createTable()) {
+		if (!Vettich\SP\db\pendingTable::createTable()) {
 			return false;
 		}
 
-		$def_options = array(
+		$def_options = [
 			// posts
 			'is_enable' => 'Y',
 			// facebook
@@ -103,24 +104,24 @@ class vettich_sp extends CModule
 			'is_twitter_enable' => 'Y',
 			// vk
 			'is_vk_enable' => 'Y',
-		);
-		foreach($def_options as $k => $v) {
+		];
+		foreach ($def_options as $k => $v) {
 			COption::SetOptionString(self::MODULE_ID, $k, $v);
 		}
 		return true;
 	}
 
-	function UnInstallDB($arParams = array())
+	public function UnInstallDB($arParams = [])
 	{
 		COption::RemoveOption(self::MODULE_ID);
 		if (!$arParams['savedata'] && \CModule::IncludeModule(self::MODULE_ID)) {
-			if(!vettich\sp\db\postTable::dropTable()) {
+			if (!vettich\sp\db\postTable::dropTable()) {
 				return false;
 			}
-			if(!vettich\sp\db\postAccountTable::dropTable()) {
+			if (!vettich\sp\db\postAccountTable::dropTable()) {
 				return false;
 			}
-			if(!vettich\sp\db\pendingTable::dropTable()) {
+			if (!vettich\sp\db\pendingTable::dropTable()) {
 				return false;
 			}
 		}
@@ -128,7 +129,7 @@ class vettich_sp extends CModule
 		return true;
 	}
 
-	function InstallEvents()
+	public function InstallEvents()
 	{
 		RegisterModuleDependences('main', 'OnAdminListDisplay', 'vettich.sp', '\Vettich\SP\Events', 'OnAdminListDisplayHandler');
 		RegisterModuleDependences('main', 'OnBeforeProlog', 'vettich.sp', '\Vettich\SP\Events', 'OnBeforePrologHandler');
@@ -140,7 +141,7 @@ class vettich_sp extends CModule
 		return true;
 	}
 
-	function UnInstallEvents()
+	public function UnInstallEvents()
 	{
 		UnRegisterModuleDependences('main', 'OnAdminListDisplay', 'vettich.sp', '\Vettich\SP\Events', 'OnAdminListDisplayHandler');
 		UnRegisterModuleDependences('main', 'OnBeforeProlog', 'vettich.sp', '\Vettich\SP\Events', 'OnBeforePrologHandler');
@@ -152,13 +153,13 @@ class vettich_sp extends CModule
 		return true;
 	}
 
-	function InstallFiles()
+	public function InstallFiles()
 	{
-		CopyDirFiles($this->MODULE_ROOT_DIR.'/install/bitrix',$_SERVER['DOCUMENT_ROOT'].'/bitrix', true, true);
+		CopyDirFiles($this->MODULE_ROOT_DIR.'/install/bitrix', $_SERVER['DOCUMENT_ROOT'].'/bitrix', true, true);
 		return true;
 	}
 
-	function UnInstallFiles()
+	public function UnInstallFiles()
 	{
 		DeleteDirFiles($this->MODULE_ROOT_DIR.'/install/bitrix/admin', $_SERVER['DOCUMENT_ROOT'].'/bitrix/admin');
 		DeleteDirFiles($this->MODULE_ROOT_DIR.'/install/bitrix/js/vettich.sp', $_SERVER['DOCUMENT_ROOT'].'/bitrix/js/vettich.sp');
@@ -167,4 +168,3 @@ class vettich_sp extends CModule
 		return true;
 	}
 }
-?>
